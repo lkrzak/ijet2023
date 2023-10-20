84fe6bdb4c6adf4c50aab1183ab3445e9222eeef
Jan Macheta <jan.macheta@embe.tech>
2023-10-20 16:12:33 +0200

    update sigfox model

--- sigfox_transaction.m
+++ sigfox_transaction.m
@@ -1,7 +1,7 @@
 function [V, TR] = sigfox_transaction(mode, app_payload_length)
 V = 3.3;
 if app_payload_length <= 0 || app_payload_length > 12
-error('invalid payload length for a single frame')
+    error('invalid payload length for a single frame')
 end
 switch mode
     case 'unidirectional'
@@ -21,7 +21,7 @@ bitrate_up = 100; % bps
 
 frame_overhead = 14 * 8; %bits minimal frame size, excluding payload
 frame_length = frame_overhead + (app_payload_length * 8);%bits
-t_tx = frame_length / bitrate_up; %seconds
+t_tx = frame_length / bitrate_up * 1e3; %ms
 TR = [
     287 10.4;  % wake up
     t_tx 27.2 % tx
@@ -36,11 +36,11 @@ end
 
 function TR = sigfox_transaction_bidirectional(app_payload_length)
 bitrate_up = 100; % bps
-bitrate_down = 600; %bps
+%bitrate_down = 600; %bps
 
 frame_overhead = 14 * 8; %bits minimal frame size, excluding payload
 frame_length = frame_overhead + (app_payload_length * 8);%bits
-t_tx = frame_length / bitrate_up; %seconds
+t_tx = frame_length / bitrate_up * 1e3 %ms
 TR = [
     287 10.4;  % wake up
     t_tx 27.2 % tx
@@ -48,7 +48,10 @@ TR = [
     t_tx 27.2 % tx replica 1
     493 1.2 % % wait for next transmission
     t_tx 27.2 % tx replica 2
-    16493 1.3 % wait for reception
-
+    16493 1.3 % wait for packet propagation
+    12690 18.5 % avg active reception window (0.387-25s)
+    1430 1.2 % wait for confirmation tx
+    1850 27 % confirmation tx
+    495 1.2 % cool down
     ];
 end
