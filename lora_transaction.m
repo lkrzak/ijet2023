a56d04c132a289f5841fdd8d77d0006699eecb7b
Lukasz <lkrzak@agh.edu.pl>
2023-10-20 13:27:01 +0000

    finished

--- lora_transaction.m
+++ lora_transaction.m
@@ -1,11 +1,18 @@
-% current model of a single transaction: each row is:
-% [time (ms) current (mA)]
+% This function returns:
+% V - nominal voltage in Volts
+% TR - transaction current (see below)
+% Q - total charge consumed by transaction in mAh
 %
-% Example = [
+% TR: current model of a single transaction
+% It is a matrix, where each row has 2 columns:
+% time (ms) current (mA)
+%
+% ExampleTR = [
 %   0.1 20;   % 20mA during 0.1ms
 %   2.5 30;   % 30mA during 2.5ms
 % ];
-
+%
+% LoRaWAN:
 % Data rates for EU863-870 band channels.
 % DR  Modulation    SF  BW      Bitrate
 % 0	  LoRa          12	125 kHz	250
@@ -15,7 +22,7 @@
 % 4	  LoRa	        8	125 kHz	3125
 % 5	  LoRa	        7	125 kHz	5470
 % 6	  LoRa	        7	250 kHz	11000
-
+%
 % Assumed PHY packet format
 % | preamble | PHDR    | PHDR_CRC | PHY payload  | CRC    |
 % | 8 bytes  | 2 bytes | 4 bytes  | variable     | 2 bytes|
@@ -24,10 +31,13 @@
 % | MHDR     | FHDR    | Fport  | FRM payload | MIC     |
 % | 1 byte   | 7 bytes | 1 byte | variable    | 4 bytes |
 %
-function [V, TR] = lora_transaction(dr, frmPayloadLen)
+% Assumed coding rate: 4/5
+%
+function [V, TR, Q] = lora_transaction(dr, frmPayloadLen)
 
 CR = 1; % assume coding rate of 4/5
 
+% based on LoRaWAN regional requirements for EU868:
 switch dr
     case 0
         SF = 12;
@@ -73,17 +83,24 @@ switch dr
         Tw2w = 0.64;
 end
 
-Tsym = (2^SF)/BW;
-Tpreamble = (8 + 4.25)*Tsym;
+% based on SX1272/3/6/7/8: LoRa Modem DesignerDs Guide AN1200.13:
+Tsym = (2^SF)/BW;             % symbol time in [s]
+Tpreamble = (8 + 4.25)*Tsym;  % preamble time in [s]
 
 tp = 8*frmPayloadLen - 4 *SF + 28 + 16;
 tp = tp / (4*(SF - 2*DE));
 tp = ceil(tp);
 tp = tp * (CR+4);
-Np = 8+max(tp,0);
-Ttx = (Tpreamble + (Np*Tsym))*1000;
+Np = 8+max(tp,0);             % number of symbols (excluding preamble)
+Ttx = (Tpreamble + (Np*Tsym))*1000; % total transmission time in [s]
 
+% assume voltage
 V = 3.3;
+
+% based on
+% Casals, L.; Mir, B.; Vidal, R.; Gomez, C. 
+% Modeling the Energy Performance of LoRaWAN. 
+% Sensors 2017, 17, 2364. https://doi.org/10.3390/s17102364
 TR = [
     168.2   22.1;   % (1) wake up
     83.8    13.3;   % (2) radio preparation
@@ -96,6 +113,10 @@ TR = [
     268     21.0;   % (9) postprocessing
     38.6    13.3;   % (10) turn off sequence
     ];
-end
 
+% calculate charge
+Q = sum(TR(:,1) .* TR(:,2));
+Q = Q / (1000*3600);
+
+end
 
