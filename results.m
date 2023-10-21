612280a8d106c05914f7258dfb3b689a9d9b0738
Lukasz <lkrzak@agh.edu.pl>
2023-10-21 17:02:36 +0000

    update

--- results.m
+++ results.m
@@ -7,6 +7,7 @@ payload_length = 16; %bytes
 
 
 i = 1;
+
 item(i).protocol = 'WMBus';
 item(i).mode = 'S1';
 i = i + 1;
@@ -22,6 +23,36 @@ i = i + 1;
 item(i).protocol = 'WMBus';
 item(i).mode = 'C1';
 i = i + 1;
+
+item(i).protocol = 'LoRaWAN';
+item(i).mode = 'DR0';
+i = i + 1;
+item(i).protocol = 'LoRaWAN';
+item(i).mode = 'DR1';
+i = i + 1;
+item(i).protocol = 'LoRaWAN';
+item(i).mode = 'DR2';
+i = i + 1;
+item(i).protocol = 'LoRaWAN';
+item(i).mode = 'DR3';
+i = i + 1;
+item(i).protocol = 'LoRaWAN';
+item(i).mode = 'DR4';
+i = i + 1;
+% item(i).protocol = 'LoRaWAN';
+% item(i).mode = 'DR5';
+% i = i + 1;
+% item(i).protocol = 'LoRaWAN';
+% item(i).mode = 'DR6';
+% i = i + 1;
+
+item(i).protocol = 'Sigfox';
+item(i).mode = 'unidirectional';
+i = i + 1;
+item(i).protocol = 'Sigfox';
+item(i).mode = 'bidirectional';
+i = i + 1;
+
 item(i).protocol = 'Actislink';
 item(i).mode = '02';
 i = i + 1;
@@ -44,14 +75,21 @@ for i = 1:size(item, 2)
     if strcmp(item(i).protocol, 'WMBus')
         [v, item(i).transaction] = wmbus_transaction(item(i).mode, payload_length);
     end
+    if strcmp(item(i).protocol, 'LoRaWAN')
+        [v, item(i).transaction] = lora_transaction(item(i).mode, payload_length);
+    end
+    if strcmp(item(i).protocol, 'Sigfox')
+        [v, item(i).transaction] = sigfox_transaction(item(i).mode, payload_length);
+    end     
     if strcmp(item(i).protocol, 'Actislink')
         [v, item(i).transaction] = actislink_transaction(item(i).mode, payload_length);
-    end
+    end    
     item(i).totalCharge = sum(prod(item(i).transaction, 2)) / (3.6e3); %uAh
 end
 
-figure;
-barh([item.totalCharge]);
+f = figure;
+f.Position = [0,0,2000, 2800];
+barh([item.totalCharge], 'BarWidth', 0.2);
 axis = gca;
 c = struct2cell(item);
 axis.YTickLabels = c(3,1,:);
