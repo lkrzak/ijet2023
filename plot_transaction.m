e4b19227c227eef591d7139a088b9cd42234da98
Lukasz Krzak <lukasz.krzak@embe.tech>
2023-10-24 09:46:43 +0200

    added varargs

--- plot_transaction.m
+++ plot_transaction.m
@@ -1,10 +1,11 @@
-function plot_transaction(tr)
+function plot_transaction(tr, varargin )
 durations = [0; tr(:, 1)];
 current = [tr(:,2); 0];
-
 t = cumsum(durations);
-stairs(t, current,'-o')
-
+if (nargin > 1)
+    stairs(t, current, varargin{1:nargin-1})
+else
+    stairs(t, current)
 xlabel('time [ms]')
 ylabel('current [mA]')
 end
